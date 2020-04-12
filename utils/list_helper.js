148fd9b068a149cf7ac7a47de05e909ca31a4d95
nambii-18 <anant.nambiar@gmail.com>
2020-04-12 18:58:28 +0530

    part4.6 done

---
+++ utils/list_helper.js
@@ -0,0 +1,44 @@
+const _ = require('lodash')
+
+const dummy = (blogs) => {
+    return 1
+}
+
+const totalLikes = (blogs) => {
+    const reducer = (sum, item) => sum + item.likes
+    return blogs.length === 0 ?
+        0 : 
+        blogs.reduce(reducer, 0)
+}
+
+const favoriteBlog = (blogs) => {
+    if(blogs.length === 0){
+        return null
+    }
+    const fav = _.orderBy(blogs, ['likes'], ['desc'])[0]
+    return {
+        title: fav.title,
+        author: fav.author,
+        likes: fav.likes
+    }
+}
+
+const mostBlogs = (blogs) => {
+    if(blogs.length === 0){
+        return null
+    }
+    const countBlogs = _.countBy(blogs, 'author')
+    const max = _.max(_.toArray(countBlogs))
+
+    return {
+        author: _.invert(countBlogs)[max],
+        blogs: max
+    }
+}
+
+module.exports = {
+    dummy,
+    totalLikes,
+    favoriteBlog,
+    mostBlogs
+}
